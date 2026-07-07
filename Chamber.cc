72a9e342b976b2de7207574ece3c9466995645dd
yetian <luyetian215@gmail.com>
2026-07-07 18:33:58 -0400

    added skeleton

---
+++ Chamber.cc
@@ -0,0 +1,14 @@
+class Chamber {
+        int row_min;
+        int row_max;
+        int col_min[];
+        int col_max[];
+    public:
+        void generate(Character &Charc) {
+        ... make some generation
+        while(...)
+        // if generated successfully
+        Charc.x = x;
+        Charc.y = y;
+        }
+}
