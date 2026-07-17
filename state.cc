df8827ebf619950684ec2297bcaca58623378d81
yetian <luyetian215@gmail.com>
2026-07-17 10:47:02 -0400

    init state.cc

---
+++ state.cc
@@ -0,0 +1,30 @@
+import Enemy;
+import Player;
+import <vector>;
+
+using namespace std;
+
+export enum class EnemyT {Human, Dward, Halfling, Elf, Orc, Merchant};
+export enum class PlayerT {Shade, Drow, Vampire, Troll, Goblin};
+
+struct Pairing {
+    int hoardRow;
+    int hoardCol;
+    int dragonRow;
+    int dragonCol;
+};
+
+struct EnemyInfo {
+    vector<Pairing> hoDrag;
+    vector<pair<EnemyT, pair<int,int>> EnemyPose;
+    pair<int,int> PlayerPos;
+}
+
+// Potion
+// 0 - RH
+// 1 - BA
+// 2 - BD
+// 3 - PH
+// 4 - WA
+// 5 - WD
+
